30a67180064dacdd7082391db3fc556ceeadb6c0
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2023-01-07 19:28:01 +0000

    set exercises reincorporated

---
+++ app/src/main/java/com/example/highfidelityprototype/AdminAccount.java
@@ -0,0 +1,14 @@
+package com.example.highfidelityprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_account);
+    }
+}
