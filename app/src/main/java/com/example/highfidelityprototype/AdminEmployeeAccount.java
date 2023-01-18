0dab6c5764ab31de90fa233d1f6881c7c5bc4b8c
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2023-01-18 10:42:52 +0000

    Admin Employee Account page incorporated + admin navbar

---
+++ app/src/main/java/com/example/highfidelityprototype/AdminEmployeeAccount.java
@@ -0,0 +1,42 @@
+package com.example.highfidelityprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class AdminEmployeeAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_employee_account);
+
+        //create buttons
+        ImageButton navlogout = findViewById(R.id.NavLogOutButton);
+        ImageButton navemployee = findViewById(R.id.NavEmployeeButton);
+        ImageButton navholiday = findViewById(R.id.NavHolidayButton2);
+        ImageButton navaccount = findViewById(R.id.NavAccountButton);
+        Button editdata = findViewById(R.id.TitleEditData);
+
+        //log out button switches activity
+        navlogout.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminEmployeeAccount.this, AdminLogin.class);
+            startActivity(intent);
+        });
+
+        //holiday button switches activity
+        navholiday.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminEmployeeAccount.this, EmployeeHoliday.class);
+            startActivity(intent);
+        });
+
+        //account button switches activity
+        navaccount.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminEmployeeAccount.this, AdminAccount.class);
+            startActivity(intent);
+        });
+    }
+}
