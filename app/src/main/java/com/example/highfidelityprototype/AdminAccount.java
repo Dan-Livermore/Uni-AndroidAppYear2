28ed4c90124385c07a78e795b8c9d200a81e5f47
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2023-01-19 12:52:15 +0000

    Added functionality to navbar

--- app/src/main/java/com/example/highfidelityprototype/AdminAccount.java
+++ app/src/main/java/com/example/highfidelityprototype/AdminAccount.java
@@ -2,7 +2,9 @@ package com.example.highfidelityprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class AdminAccount extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class AdminAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_account);
+
+
+        //create buttons
+        ImageButton navlogout = findViewById(R.id.NavLogOutButton5);
+        ImageButton navemployee = findViewById(R.id.NavEmployeeButton5);
+        ImageButton navholiday = findViewById(R.id.NavHolidayButton5);
+        ImageButton navaccount = findViewById(R.id.NavAccountButton5);
+
+
+        //log out button switches activity
+        navlogout.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminAccount.this, AdminLogin.class);
+            startActivity(intent);
+        });
+
+        //employee button switches activity
+        navemployee.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminAccount.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        //holiday button switches activity
+        navholiday.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminAccount.this, EmployeeHoliday.class);
+            startActivity(intent);
+        });
+
+        //account button switches activity
+        navaccount.setOnClickListener(view ->{
+            Intent intent = new Intent(AdminAccount.this, EmployeeAccount.class);
+            startActivity(intent);
+        });
     }
 }
